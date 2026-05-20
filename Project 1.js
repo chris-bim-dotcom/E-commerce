6969acae6d5e04b1d3129ff1df410541c585aa6c
chris-bim-dotcom <chrisellashevell@sekolahbim.sch.id>
2026-05-20 11:32:38 +0700

    Project 1.js

--- Project 1.js
+++ Project 1.js
@@ -1,2 +1,20 @@
 function Alert() {
   alert("こんにちは、神拉麺です！🍜");
+
+document.getElementById('OK').onclick=function(){
+	var name=document.getElementById('name').value.trim();
+	if (name===""){
+		name="(Please fill in your name)";
+	}
+	var kelas=document.getElementById('kelas').value.trim();
+	if (kelas===""){
+		kelas="(Please fill in your contact)";
+	}
+	
+	var one=document.getElementById('1').checked ? "Product 1":"";
+	var one=document.getElementById('2').checked ? "Product 2":"";
+	var one=document.getElementById('').checked ? "":"";
+	
+	var hasilText="Nama:" + nama + "Kelas:" + kelas + "\nUmur:" + 1 || 2 || ...;
+	document.getElementById('hasil').innerText=hasilText;
+}
